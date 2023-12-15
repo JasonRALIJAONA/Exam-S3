7485deb1392e307e735fada1c77615f95c869e90
JasonRALIJAONA <jasonralijaona@gmail.com>
2023-12-15 12:58:47 +0100

    ampovoany

--- inc/fonctions.php
+++ inc/fonctions.php
@@ -12,4 +12,55 @@ function insererAleat ($dateAchat,$produit,$prixUnitaire,$quantite){
     $stmt->execute();
 }
 
+function getPrixInitial(){
+    $sql="SELECT * FROM devise";
+
+    // Get the PDO connection
+    $pdo = mySQLconnection();
+
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute();
+
+    // Fetch all rows as an associative array
+    $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+    return $result;    
+}
+
+function prochainJour($dateString) {
+    $date = new DateTime($dateString);
+
+    // Ajoute un jour à la date
+    $date->add(new DateInterval('P1D'));
+
+    // Retourne la nouvelle date sous forme de chaîne
+    return $date->format('Y-m-d');
+}
+
+function insertAleat($debut,$range,$ligneAchat,$minAchat,$maxAchat,$ligneVente,$minVente,$maxVente) {
+    $prixbase=getPrixInitial();
+    
+    $aleatAchat=rand($minAchat , $maxAchat);
+    $aleatVente=rand($minVente , $maxVente);
+
+    // Get the PDO connection
+    $pdo = mySQLconnection();
+
+    for ($i=0; $i <$range ; $i++) {
+
+        //Insertion ligne achat 
+        for ($j=0; $j <$ligneAchat ; $j++) { 
+            
+        }
+        
+        //insertion ligne vente
+        for ($k=0; $k <$ligneVente ; $k++) { 
+            # code...
+        }
+
+        $debut=prochainJour($debut);
+    }
+
+}
+
 ?>
